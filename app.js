79788d26163834869b54ff228a60d80c5ab2f76d
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-04 18:06:37 +0500

    coding

--- app.js
+++ app.js
@@ -15,3 +15,7 @@ class Students {
 
 const student1 = new Students ("ali",20)
 student1.Studentsdata()
+
+// Abstruction First piller of oop 
+// absraction me kisis user ko wo chizen dete hein jis ki ose zarorat ho bas baki piche kiya kam hoa hy os ka nai bta te wo hide rehta hy user se 
+
