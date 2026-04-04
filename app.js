19af1a4a38c19330e37b65f23e34c8a319ca582a
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-04 18:10:35 +0500

    code

--- app.js
+++ app.js
@@ -19,3 +19,7 @@ student1.Studentsdata()
 // Abstruction First piller of oop 
 // absraction me kisis user ko wo chizen dete hein jis ki ose zarorat ho bas baki piche kiya kam hoa hy os ka nai bta te wo hide rehta hy user se 
 
+function area (length){
+  return 5 * length
+}
+console.log(area(10))
