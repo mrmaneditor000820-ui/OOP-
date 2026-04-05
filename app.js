a8a4dd05a40bca27ce3718252e298ed2672d2f51
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-05 16:36:11 +0500

    coding

--- app.js
+++ app.js
@@ -1,26 +1,90 @@
- 
-class Students {
-    constructor (sname,sage,sclass,sid,sroll){
-        this.sname=sname
-        this.sage=sage
-        this.sclass=sclass
-        this.sid=sid
-        this.sroll=sroll
-    }
 
-    Studentsdata(){
-        console.log(`student name is ${this.sname} and age is ${this.sage}`)
+// class Students {
+//     constructor (sname,sage,sclass,sid,sroll){
+//         this.sname=sname
+//         this.sage=sage
+//         this.sclass=sclass
+//         this.sid=sid
+//         this.sroll=sroll
+//     }
+
+//     Studentsdata(){
+//         console.log(`student name is ${this.sname} and age is ${this.sage}`)
+//     }
+// }
+
+// const student1 = new Students ("ali",20)
+// student1.Studentsdata()
+
+// // Abstruction First piller of oop 
+// // absraction me kisis user ko wo chizen dete hein jis ki ose zarorat ho bas baki piche kiya kam hoa hy os ka nai bta te wo hide rehta hy user se 
+
+// function area (length){
+//   return 5 * length      
+// }
+// console.log(area(10))
+
+
+
+
+// inheritance
+
+// object ke sath inheritancs
+
+// const obj = {
+//     name:"ali" ,
+//     age:20,
+// };
+// Object.prototype.details = function main (){
+//     console.log(`my name is ${this.name}`)
+// };
+// obj.details()
+
+
+// function ke sath inheritance
+
+// function studentdata (name){
+//     this.name = name
+// }
+// studentdata.prototype.details = function(name){
+//     console.log(`student name is ${this.name}`)
+// }
+// const mydata = new studentdata("ali")
+// mydata.details()
+
+
+// class ke sath inheritance 
+
+// class Parentclass{
+//    constructor(name , age){
+//     this.name = name
+//     this.age = age
+//    }
+//    details(){
+//     console.log(`parent class name is ${this.name} and age is ${this.age}`)
+//    }
+// }
+// class Childclass extends Parentclass{
+//     constructor(name , age){
+//       super(name,age)
+//     }
+// }
+// const obj = new Childclass("ali" , 20)
+// obj.details()
+
+// factory function 
+
+function MainFunction(name, age) {
+    return {
+        name: name,
+        age: age,
+        details: function () {
+            console.log(`name is ${this.name} and age ${this.age}`)
+        }
     }
 }
 
-const student1 = new Students ("ali",20)
-student1.Studentsdata()
-
-// Abstruction First piller of oop 
-// absraction me kisis user ko wo chizen dete hein jis ki ose zarorat ho bas baki piche kiya kam hoa hy os ka nai bta te wo hide rehta hy user se 
-
-function area (length){
-  return 5 * length      
-}
-console.log(area(10))
-
+const function1 = new MainFunction("ali", 20)
+const function2 = new MainFunction("khan", 30)
+function1.details()
+function2.details()
